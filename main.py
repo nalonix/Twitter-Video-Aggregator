6582435b181d272b69dbc3e6feeae36cd158a825
Naol Fekadu <nf.naol9@gmail.com>
2025-10-09 02:36:23 +0300

    moved data source

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-# main.py or send_x_videos.py
 from db import *  # establishes connection
 
 import time
 from get_liked_videos import get_liked_videos
+from scrapped.grab_vids import grab_vids
 from send_x_videos import send_x_videos
 
 
 if __name__ == "__main__":
-    liked_data = get_liked_videos(max_results=30)
+    liked_data = grab_vids(max_results=5)
     print("👺 Found liked videos: ", liked_data)
+        
+    
 
-    for vid in liked_data.get("videos", []):
-        try:
-            send_x_videos(vid)
-            time.sleep(3)
-        except Exception as e:
-            print(f"❌ Failed to send video for tweet {vid.get('tweet_id')}\nReason: {e}")
+    # for vid in liked_data.get("videos", []):
+    #     try:
+    #         send_x_videos(vid)
+    #         time.sleep(3)
+    #     except Exception as e:
+    #         print(f"❌ Failed to send video for tweet {vid.get('tweet_id')}\nReason: {e}")
